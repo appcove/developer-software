ed532637923af1cb8b4ac59a1cc267c8d786f2b7
Dilec <dilec.padovani@gmail.com>
2022-09-22 21:35:51 +0200

    Added `fd-find`

---
+++ build_scripts/fd.py
@@ -0,0 +1,33 @@
+import lsb_release
+import os
+from pathlib import Path
+import shutil
+from common import *
+
+# These are the equivalent of the control file attributes
+ARCHITECTURE = "amd64"
+# package should be the same as the source project folder
+PACKAGE = "fd"
+VERSION = "8.4.0"
+MAINTAINER = "AppCove <developer-software@appcove.com>"
+DEPENDS = ""
+HOMEPAGE = "https://github.com/sharkdp/fd"
+DESCRIPTION = "fd is a program to find entries in your filesystem. It is a simple, fast and user-friendly alternative to find"
+
+UBUNTU_VERSION = lsb_release.get_distro_information()["RELEASE"]
+
+
+if __name__ == '__main__':
+    install_rust()
+    os.chdir(f"sources/{PACKAGE}")
+    cargo_build_project()
+
+    BUILD_FOLDER = f"../../temp/{PACKAGE}_{VERSION}custom{UBUNTU_VERSION}_{ARCHITECTURE}"
+    Path(f'{BUILD_FOLDER}/usr/bin').mkdir(parents=True, exist_ok=True)
+    shutil.copy(f"./target/release/fd-find",
+                Path(f'{BUILD_FOLDER}/usr/bin/fd'))
+    Path(f'{BUILD_FOLDER}/DEBIAN').mkdir(parents=True, exist_ok=True)
+    os.chdir(f'{BUILD_FOLDER}')
+    write_control_file(BUILD_FOLDER, PACKAGE, VERSION, UBUNTU_VERSION,
+                       MAINTAINER, DEPENDS, ARCHITECTURE, HOMEPAGE, DESCRIPTION)
+    create_deb_package(f"{BUILD_FOLDER}")
