f0d54f1c4b9dbc7d19c06f8c8cbfb205df53d1cf
Dilec <dilec.padovani@gmail.com>
2022-10-06 23:16:29 +0200

    try

--- build_scripts/release.py
+++ build_scripts/release.py
@@ -43,7 +43,7 @@ sudo curl -s --compressed -o /etc/apt/sources.list.d/appcove-developer-software.
     os.chdir(f'{BUILD_FOLDER}')
     write_control_file(BUILD_FOLDER, f"asd-{PACKAGE}", VERSION, UBUNTU_VERSION,
                        MAINTAINER, DEPENDS, ARCHITECTURE, HOMEPAGE, DESCRIPTION)
-    os.chmod(f'{BUILD_FOLDER}/DEBIAN/postinst', 555)
+    os.chmod(f'{BUILD_FOLDER}/DEBIAN/postinst', 0o775)
 
     st = os.stat(f'{BUILD_FOLDER}/DEBIAN/postinst')
     oct_perm = oct(st.st_mode)
