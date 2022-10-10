d64d99cc8720823f3f40acb69da651bdd2a4ba70
Dilec <dilec.padovani@gmail.com>
2022-10-11 01:24:46 +0200

    Added cache

--- build_scripts/main.py
+++ build_scripts/main.py
@@ -1,3 +1,4 @@
+from functools import cache
 import requests
 import json
 import re
@@ -5,7 +6,9 @@ from pathlib import Path
 import subprocess
 import ubuntu_folder
 from common import install_rust
-
+import yaml
+import shutil
+import glob
 
 # def get_latest_version_and_deb_file(git_repo_url: str):
 
@@ -38,22 +41,52 @@ from common import install_rust
 
 
 if __name__ == "__main__":
+
     install_rust()
+
     with open("included_tools.json", "r") as config_file:
         config_json = json.load(config_file)
         # precompiled_tools_urls = config_json["precompiled_tools"]
         to_compile_tools = config_json["to_be_compiled_tools"]
 
-    # download precompiled tools' debs
     Path(f'temp').mkdir(parents=True, exist_ok=True)
+
+    # TODO: check it
+    subprocess.run(
+        ["git", "checkout", "website", "--", "cache.yaml"], capture_output=True)
+
+    with open(r'cache.yaml') as cache_file:
+        cached_submodules_hashes = yaml.full_load(cache_file)
+
+    # run custom scripts
+    for tool_name in to_compile_tools:
+        current_submodule_hash = subprocess.run(
+            ["git", "submodule", "status", f"sources/{tool_name}"], capture_output=True)
+        print(current_submodule_hash)
+        print(cached_submodules_hashes[tool_name])
+
+        if current_submodule_hash == cached_submodules_hashes[tool_name]:
+            # TODO: (check it) take deb from binary and insert into temp
+            cache_build_deb = subprocess.run(
+                f"git checkout website:ubuntu/dists/jammy/main/binary-amd64 -- $(git ls-tree --name-only -r website:ubuntu/dists/jammy/main/binary-amd64 | egrep -e '^.*bat.*.deb$')", shell=True, capture_output=True)
+
+            for deb_file in glob.glob(r'*.deb'):
+                shutil.move(deb_file, "temp")
+
+        else:
+            subprocess.check_output(
+                ["python3", f"build_scripts/{tool_name}.py"])
+            cached_submodules_hashes[tool_name] == current_submodule_hash
+
+    with open(r'cache.yaml', 'w+') as cache_file:
+        yaml.dump(cached_submodules_hashes, cache_file)
+
+    ubuntu_folder.init_ubuntu_folder()
+
+    # idea for future
+    # download precompiled tools' debs
     # for tool_url in precompiled_tools_urls:
     #     (package_name, deb_download_url) = get_latest_version_and_deb_file(tool_url)
     #     print((package_name, deb_download_url))
     #     response = requests.get(deb_download_url)
     #     open(f"temp/{package_name}.deb", "wb+").write(response.content)
-
-    # run custom scripts
-    for tool_name in to_compile_tools:
-        subprocess.check_output(["python3", f"build_scripts/{tool_name}.py"])
-
-    ubuntu_folder.init_ubuntu_folder()
