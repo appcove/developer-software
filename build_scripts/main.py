46f70ab12ce381e90976fbfdf35f0b08608f8b99
Dilec <dilec.padovani@gmail.com>
2022-10-11 02:15:16 +0200

    try

--- build_scripts/main.py
+++ build_scripts/main.py
@@ -70,7 +70,7 @@ if __name__ == "__main__":
         print(current_submodule_hash)
         print(cached_submodules_hashes.get(tool_name))
 
-        if cached_submodules_hashes.get(tool_name) in current_submodule_hash:
+        if cached_submodules_hashes.get(tool_name) == current_submodule_hash.split()[0].removeprefix('-'):
             # TODO: (check it) take deb from binary and insert into temp
             cache_build_deb = subprocess.run(
                 f"git checkout website:ubuntu/dists/jammy/main/binary-amd64 -- $(git ls-tree --name-only -r website:ubuntu/dists/jammy/main/binary-amd64 | egrep -e '^.*bat.*.deb$')", shell=True, capture_output=True).stdout
