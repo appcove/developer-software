25bc049f5c6f400e86c46bc989be578f748a3185
Dilec <dilec.padovani@gmail.com>
2022-10-12 22:34:43 +0200

    Translated into a dataclass type structure

--- build_scripts/common.py
+++ build_scripts/common.py
@@ -1,12 +1,79 @@
-from importlib.resources import Package
 import subprocess
 from subprocess import check_output, STDOUT, CalledProcessError
+from tkinter.messagebox import NO
 
 import lsb_release
 import os
 from pathlib import Path
 import shutil
-from common import *
+from dataclasses import dataclass, field
+
+# frozen set the class to be read_only
+
+
+@dataclass(order=True, kw_only=True)
+class AdsPackage:
+    package_name: str
+    version: str
+    maintainer: str = field(
+        init=False, default="AppCove <developer-software@appcove.com>")
+    depends: str = ""
+    homepage: str
+    description: str
+    arch: str = field(init=False, default="amd64")
+
+
+@dataclass(order=True, kw_only=True)
+class SimpleRust(AdsPackage):
+    # binaries names can be different then package name. Default = package_name
+    binaries_name: list[str] = None
+
+    def __post_init__(self):
+        if self.binaries_name is None:
+            self.binaries_name = [self.package_name]
+
+    def build(self):
+        ubuntu_version = lsb_release.get_distro_information()["RELEASE"]
+        os.chdir(f"sources/{self.package_name}")
+        cargo_build_project()
+
+        BUILD_FOLDER = f"../../temp/ads-{self.package_name}_{self.version}custom{ubuntu_version}_{self.arch}"
+        Path(f'{BUILD_FOLDER}/opt/ads/bin').mkdir(parents=True, exist_ok=True)
+        for bin in self.binaries_name:
+            shutil.copy(f"./target/release/{bin}",
+                        Path(f'{BUILD_FOLDER}/opt/ads/bin'))
+        Path(f'{BUILD_FOLDER}/DEBIAN').mkdir(parents=True, exist_ok=True)
+        os.chdir(f'{BUILD_FOLDER}')
+        write_control_file(BUILD_FOLDER, super(),  ubuntu_version)
+        create_deb_package(f"{BUILD_FOLDER}")
+
+
+@dataclass(order=True, kw_only=True)
+class AdsRelease(AdsPackage):
+
+    def build(self):
+        ubuntu_version = lsb_release.get_distro_information()["RELEASE"]
+        os.chdir(f"sources/bat")
+        BUILD_FOLDER = f"../../temp/ads-{self.package_name}_{self.version}custom{ubuntu_version}_{self.arch}"
+
+        # add path to bins
+        Path(f'./{BUILD_FOLDER}/etc/profile.d').mkdir(parents=True, exist_ok=True)
+        with open(f'{BUILD_FOLDER}/etc/profile.d/10-ads-release.sh', "w") as release_file:
+            release_file.write("export PATH=\"$PATH:/opt/ads/bin\"")
+
+            # add key and list file
+        Path(f'{BUILD_FOLDER}/DEBIAN').mkdir(parents=True, exist_ok=True)
+        with open(f'{BUILD_FOLDER}/DEBIAN/postinst', "w") as release_file:
+            release_file.write("""
+curl -s --compressed "https://appcove.github.io/developer-software/ubuntu/KEY.gpg" | sudo gpg --batch --yes --dearmor -o /usr/share/keyrings/appcove-developer-software.gpg
+sudo curl -s --compressed -o /etc/apt/sources.list.d/appcove-developer-software.list \"https://appcove.github.io/developer-software/ubuntu/dists/jammy/appcove-developer-software.list\"""")
+
+        Path(f'{BUILD_FOLDER}/DEBIAN').mkdir(parents=True, exist_ok=True)
+        os.chdir(f'{BUILD_FOLDER}')
+        write_control_file(BUILD_FOLDER, super(),  ubuntu_version)
+        os.chmod(f'{BUILD_FOLDER}/DEBIAN/postinst', 0o775)
+
+        create_deb_package(f"{BUILD_FOLDER}")
 
 
 def install_rust():
@@ -20,17 +87,17 @@ def cargo_build_project():
     subprocess.run("cargo build --release", shell=True)
 
 
-def write_control_file(path, PACKAGE, VERSION, UBUNTU_VERSION, MAINTAINER, DEPENDS, ARCHITECTURE, HOMEPAGE, DESCRIPTION):
+def write_control_file(path, package_info: AdsPackage, UBUNTU_VERSION):
     print(f"{path}/DEBIAN/control")
 
     with open(f"{path}/DEBIAN/control", 'w') as f:
-        f.write(f'Package: ads-{PACKAGE}\n')
-        f.write(f'Version: {VERSION}custom{UBUNTU_VERSION}\n')
-        f.write(f'Maintainer: {MAINTAINER}\n')
-        f.write(f'Depends: {DEPENDS}\n')
-        f.write(f'Architecture: {ARCHITECTURE}\n')
-        f.write(f'Homepage: {HOMEPAGE}\n')
-        f.write(f'Description: {DESCRIPTION}\n')
+        f.write(f'Package: ads-{package_info.package_name}\n')
+        f.write(f'Version: {package_info.version}custom{UBUNTU_VERSION}\n')
+        f.write(f'Maintainer: {package_info.maintainer}\n')
+        f.write(f'Depends: {package_info.depends}\n')
+        f.write(f'Architecture: {package_info.arch}\n')
+        f.write(f'Homepage: {package_info.homepage}\n')
+        f.write(f'Description: {package_info.description}\n')
 
 
 def create_deb_package(path):
--- build_scripts/main.py
+++ build_scripts/main.py
@@ -5,96 +5,128 @@ import re
 from pathlib import Path
 import subprocess
 import ubuntu_folder
-from common import install_rust
+from common import install_rust, AdsPackage, SimpleRust
 import yaml
 import shutil
 import glob
 
-# def get_latest_version_and_deb_file(git_repo_url: str):
+packages = [
+    SimpleRust(
+        package_name="bat",
+        version="0.22.1",
+        homepage="https://github.com/sharkdp/bat",
+        description="A cat clone with syntax highlighting and Git integration",
+    ),
+    SimpleRust(
+        package_name="git-excess",
+        binaries_name=["git-sdif", "git-srep", "git-embed", "egit"],
+        version="1.0.1",
+        homepage="https://github.com/appcove/git-excess",
+        description="AppCove's internal git tool",
+    ),
+    SimpleRust(
+        package_name="pastel",
+        version="0.9.0",
+        homepage="https://github.com/sharkdp/pastel/tree/3719824a56fb9eb92eb960068e513b95486756a7",
+        description="pastel is a command-line tool to generate, analyze, convert and manipulate colors. It supports many different color formats and color spaces like RGB (sRGB), HSL, CIELAB, CIELCh as well as ANSI 8-bit and 24-bit representations.",
+    ),
+    SimpleRust(
+        package_name="fd",
+        version="8.4.0",
+        homepage="https://github.com/sharkdp/fd",
+        description="fd is a program to find entries in your filesystem. It is a simple, fast and user-friendly alternative to find",
+    ),
+    SimpleRust(
+        package_name="procs",
+        version="0.13.2",
+        homepage="https://github.com/dalance/procs",
+        description="modern replacement for ps",
+    ),
+    SimpleRust(
+        package_name="grex",
+        version="1.4.0",
+        homepage="https://github.com/pemistahl/grex",
+        description="grex is meant to simplify the tedious task of creating regular expressions. It does so by automatically generating a single regular expression from user-provided test cases.",
+    ),
+    SimpleRust(
+        package_name="broot",
+        version="1.16.0",
+        homepage="https://github.com/Canop/broot",
+        description="A better way to navigate directories",
+    ),
+    SimpleRust(
+        package_name="exa",
+        version="0.10.1",
+        homepage="https://github.com/ogham/exa",
+        description="exa is a modern replacement for the venerable file-listing command-line program ls",
+    ),
+    SimpleRust(
+        package_name="sd",
+        version="0.7.6",
+        homepage="https://github.com/chmln/sd",
+        description="sd is an intuitive find & replace CLI.",
+    ),
+    SimpleRust(
+        package_name="dust",
+        version="0.8.3",
+        homepage="https://github.com/bootandy/dust",
+        description="Dust is a more intuitive version of `du`, used for displaying disk usage statistics.",
+    ),
+    SimpleRust(
+        package_name="ripgrep",
+        binaries_name=["rg"],
+        version="13.0.0",
+        homepage="https://github.com/BurntSushi/ripgrep",
+        description="ripgrep is a line-oriented search tool that recursively searches the current directory for a regex pattern.",
+    ),
+    SimpleRust(
+        package_name="bottom",
+        binaries_name=["btm"],
+        version="1.16.0",
+        homepage="https://github.com/Canop/broot",
+        description="A better way to navigate directories",
+    )
 
-#     package_creator_and_name = re.search(
-#         "https://github.com/([^\n]+)", git_repo_url)
-#     package_creator_and_name = package_creator_and_name.group(1)
-
-#     package_latest_asset = f"https://api.github.com/repos/{package_creator_and_name}/releases/latest"
-
-#     package_name = re.search(
-#         "https://api.github.com/repos/.*/([^\n]+)/releases/latest", package_latest_asset)
-
-#     package_name = package_name.group(1)
-#     latest_version_info = requests.get(package_latest_asset).json()
-
-#     ubuntu_deb_assets = [asset["browser_download_url"]
-#                          for asset in latest_version_info["assets"] if re.match(f"{package_name}_.*_amd64.deb",  asset["name"])]
-
-#     print("xxx")
-#     if len(ubuntu_deb_assets):
-#         package_version = re.search(
-#             "https://github.com/.*/.*/releases/download/v([^\n]+)/.*", ubuntu_deb_assets[0])
-
-#         package_version = package_version.group(1)
-#         print("io sono")
-#         return (package_name, ubuntu_deb_assets[0])
-#     else:
-#         raise RuntimeError(
-#             f'could not get asset download link from {git_repo_url} ')
+]
 
 
-if __name__ == "__main__":
+install_rust()
 
-    install_rust()
 
-    with open("included_tools.json", "r") as config_file:
-        config_json = json.load(config_file)
-        # precompiled_tools_urls = config_json["precompiled_tools"]
-        to_compile_tools = config_json["to_be_compiled_tools"]
+Path(f'temp').mkdir(parents=True, exist_ok=True)
 
-    Path(f'temp').mkdir(parents=True, exist_ok=True)
+subprocess.run(
+    ["git", "checkout", "remotes/origin/website", "--", "cache.yaml"])
+try:
+    with open(r'cache.yaml') as cache_file:
+        cached_submodules_hashes = yaml.full_load(cache_file)
+        print(cached_submodules_hashes)
+except FileNotFoundError:
+    cached_submodules_hashes = {}
 
-    subprocess.run(
-        ["git", "checkout", "remotes/origin/website", "--", "cache.yaml"])
+# run custom scripts
+for tool in packages:
+    current_submodule_hash = subprocess.run(
+        ["git", "submodule", "status", f"sources/{tool.package_name}"], capture_output=True).stdout
     try:
-        with open(r'cache.yaml') as cache_file:
-            cached_submodules_hashes = yaml.full_load(cache_file)
-            print(cached_submodules_hashes)
-    except FileNotFoundError:
-        cached_submodules_hashes = {}
+        current_submodule_hash = str(
+            current_submodule_hash.decode("utf-8")).split()[0]
+    except IndexError:
+        current_submodule_hash = ""
 
-    # run custom scripts
-    for tool_name in to_compile_tools:
-        current_submodule_hash = subprocess.run(
-            ["git", "submodule", "status", f"sources/{tool_name}"], capture_output=True).stdout
-        try:
-            current_submodule_hash = str(
-                current_submodule_hash.decode("utf-8")).split()[0]
-        except IndexError:
-            current_submodule_hash = ""
+    if cached_submodules_hashes.get(tool.package_name) == current_submodule_hash and current_submodule_hash != "":
+        print(f"{tool.package_name} from cache")
+        cache_build_deb = subprocess.run(
+            f"git checkout remotes/origin/website:ubuntu/dists/jammy/main/binary-amd64 -- $(git ls-tree --name-only -r remotes/origin/website:ubuntu/dists/jammy/main/binary-amd64 | egrep -e '^.*{tool.package_name}.*.deb$')", shell=True, capture_output=True).stdout
+        cache_build_deb = str(cache_build_deb)
+        for deb_file in glob.glob(r'*.deb'):
+            shutil.move(deb_file, "temp")
+    else:
+        tool.simple_rust_build()
+        cached_submodules_hashes[tool.package_name] = str(
+            current_submodule_hash)
 
-        if cached_submodules_hashes.get(tool_name) == current_submodule_hash and current_submodule_hash != "":
-            print(f"{tool_name} from cache")
-            cache_build_deb = subprocess.run(
-                f"git checkout remotes/origin/website:ubuntu/dists/jammy/main/binary-amd64 -- $(git ls-tree --name-only -r remotes/origin/website:ubuntu/dists/jammy/main/binary-amd64 | egrep -e '^.*{tool_name}.*.deb$')", shell=True, capture_output=True).stdout
-            cache_build_deb = str(cache_build_deb)
-            for deb_file in glob.glob(r'*.deb'):
-                shutil.move(deb_file, "temp")
-        else:
-            subprocess.check_output(
-                ["python3", f"build_scripts/{tool_name}.py"])
-            cached_submodules_hashes[tool_name] = str(current_submodule_hash)
+with open(r'cache.yaml', 'w+', encoding='utf8') as cache_file:
+    yaml.dump(cached_submodules_hashes, cache_file)
 
-    with open(r'cache.yaml', 'w+', encoding='utf8') as cache_file:
-        yaml.dump(cached_submodules_hashes, cache_file)
-
-    ubuntu_folder.init_ubuntu_folder()
-
-    # idea for future
-    # download precompiled tools' debs
-    # for tool_url in precompiled_tools_urls:
-    #     (package_name, deb_download_url) = get_latest_version_and_deb_file(tool_url)
-    #     print((package_name, deb_download_url))
-    #     response = requests.get(deb_download_url)
-    #     open(f"temp/{package_name}.deb", "wb+").write(response.content)
-
-
-# https://raw.githubusercontent.com/appcove/developer-software/website/cache.yaml
-# https://raw.githubusercontent.com/appcove/developer-software/website/ubuntu/dists/jammy/main/binary-amd64/
+ubuntu_folder.init_ubuntu_folder()
