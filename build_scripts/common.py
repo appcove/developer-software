78d7362531904baa18f4bd57499ac3e6ed9ebe3b
Dilec Padovani <39855725+DilecPadovani@users.noreply.github.com>
2022-10-20 00:12:07 +0200

    Update common.py

--- build_scripts/common.py
+++ build_scripts/common.py
@@ -29,9 +29,12 @@ class Package(object):
 
             # infer the package name from the class name
             print(f"Subclass class name : {cls.__name__}")
+            
 
             if not hasattr(cls, 'package_name'):
                 cls.package_name = cls.__name__
+            print(f'after change {cls.package_name}')
+
 
             if cls.package_name in PackageMap:
                 raise KeyError(
@@ -284,7 +287,7 @@ def BuildAll():
     cached_submodules_hashes = Package.get_cached_tools()
     for package_class in PackageMap.values():
         # Create instance
-        print(package_class.__name__)
+        print(package_class.package_name)
         package = package_class()
 
         if package.is_cached(cached_submodules_hashes):
